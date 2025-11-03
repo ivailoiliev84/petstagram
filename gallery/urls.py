719faac8c0be785bc97413a48591ca24c6910e0e
Ivaylo Iliev <ivohordda@gmail.com>
2025-11-03 22:07:36 +0200

    Implement Like Dislike functionality!

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from gallery.views import Gallery, CreatePost , PostDetails, EditPost, DeleteComment, DeletePost
+from gallery.views import (Gallery, 
+                           CreatePost , 
+                           PostDetails, 
+                           EditPost, 
+                           DeleteComment, 
+                           DeletePost, 
+                           PostLikeView, 
+                           PostDislikeView
+                           )
 urlpatterns = [
     path('list/', Gallery.as_view(), name='gallery'),
     path('create-post/', CreatePost.as_view(), name='create_post'),
     path('edit-post/<int:pk>', EditPost.as_view(), name='edit_post'),
     path('post-details/<int:pk>', PostDetails.as_view(), name='post_details'),
     path('delete-comment/<int:pk>', DeleteComment.as_view(), name='delete_comment'),
-    path('delete-post/<int:pk>', DeletePost.as_view(), name='delete_post')
-
+    path('delete-post/<int:pk>', DeletePost.as_view(), name='delete_post'),
+    path('post-like/<int:pk>', PostLikeView.as_view(), name='post_like'),
+    path('post-dislike/<int:pk>', PostDislikeView.as_view(), name='post_dislike'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.db.models import Count, OuterRef
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from gallery.forms import CreatePostForm, CommentForm
-from gallery.models import Post, PostComment
+from gallery.models import Post, PostComment, PostLike, PostDislike
 # Create your views here.
 
 
@@ -13,9 +14,20 @@ class Gallery(LoginRequiredMixin, View):
     gallery_template = "gallery/gallery_main.html"
 
     def get(self, request):
-        posts = Post.objects.all()
+        # posts = Post.objects.all()
+   
+        posts= (
+            Post.objects.select_related('user')
+            .annotate(
+               comments_count = Count('comments', distinct=True),
+               likes_count = Count('likes', distinct=True),
+               dislikes_count = Count('dislikes', distinct=True)
+            )
+        )
         context = {'posts': posts}
+
         return render(request, self.gallery_template, context)
+
     
 class CreatePost(LoginRequiredMixin, View):
     create_post_template = 'gallery/create_post.html'
@@ -44,9 +56,11 @@ class PostDetails(LoginRequiredMixin, View):
         form = CommentForm()
 
         comments = post.comments.select_related('user', 'user__userprofile').order_by('-created_at')
+        likes_count = PostLike.objects.filter(post=post).count()
         context = {'form':form, 
                    'post': post, 
                    'comments': comments,
+                   'likes_count': likes_count
                    }
         
         return render(request, self.post_details_template, context)
@@ -89,7 +103,6 @@ class DeleteComment(LoginRequiredMixin, View):
         return redirect('post_details', pk=post.pk)
         
         
-        
 class DeletePost(LoginRequiredMixin, View):
     
     def get(self, request, pk):
@@ -97,3 +110,31 @@ class DeletePost(LoginRequiredMixin, View):
         post.delete()
         return redirect('gallery')
 
+
+class PostLikeView(LoginRequiredMixin, View):
+
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        like_qs = PostLike.objects.filter(user=request.user, post=post)
+        if like_qs.exists():
+            like_qs.delete()
+        else:
+            PostLike.objects.create(user=request.user, post=post)
+            PostDislike.objects.filter(user=request.user, post=post).delete()
+        return redirect('gallery')
+   
+
+class PostDislikeView(LoginRequiredMixin, View):
+    
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        dislike_qs = PostDislike.objects.filter(user=request.user, post=post)
+
+        if dislike_qs.exists():
+            dislike_qs.delete()
+        else:
+            PostDislike.objects.create(user=request.user, post=post)
+            PostLike.objects.filter(user=request.user, post=post).delete()
+
+        return redirect('gallery')
+    
