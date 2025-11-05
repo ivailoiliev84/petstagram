1d27ed6ccc97986543a46967d2ea86c90139066d
Ivaylo Iliev <ivohordda@gmail.com>
2025-11-05 08:18:49 +0200

    FIX

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 # Create your views here.
 
+@method_decorator(cache_page(60 * 5), name='dispatch')   # 5 minutes
 class HomeView(View):
 
     def get(self, request):
