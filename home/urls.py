bc21e251fc072f5c9a3b2058f5059ba00b2f2821
ivohorda <iiliev@vmobile.eu>
2025-11-10 16:16:32 +0200

    Develop advice part from explore section

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from home.views import HomeView
+from home.views import HomeView, ExploreView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('explore', ExploreView.as_view(), name='explore'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,11 +4,18 @@ from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 # Create your views here.
 
-@method_decorator(cache_page(60 * 5), name='dispatch')   # 5 minutes
-class HomeView(View):
 
+class HomeView(View):
+    home_template = 'home/home.html'
+    
     def get(self, request):
         user = request.user
-        print(user.is_authenticated)
-        return render(request, 'home/home.html', {'user': user})
+        return render(request, self.home_template, {'user': user})
     
+@method_decorator(cache_page(60 * 5), name='dispatch')   # 5 minutes
+class ExploreView(View):
+    explore_template = 'home/explore.html'
+    
+    def get(self, request):
+        
+        return render(request, self.explore_template)
